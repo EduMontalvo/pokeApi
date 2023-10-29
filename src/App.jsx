89a6257baa5e8a2825fa806a596033354493bdd0
Edu Montalvo <kaedu18@gmail.com>
2023-10-28 20:01:32 -0500

    Se termino de dar formato al card

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ const App = () => {
     <>
       <Header/>
       <Card number="#001" name="bulba" type1="tipo1" type2="tipo2" weight="7kg" height="1.8m"/>
+      <Card number="#001" name="bulba" type1="tipo1" type2="tipo2" weight="7kg" height="1.8m"/>
     </>
   )
 }
