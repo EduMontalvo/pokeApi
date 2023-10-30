561f2d0fbea80c3d66b7757121f14507fa9627be
Edu Montalvo <kaedu18@gmail.com>
2023-10-30 12:21:56 -0500

    Se corrigieron algunos detalles en el card y el header

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,23 @@
 import Header from "./components/Header"
 import Card from "./components/Card"
+import "./Styles/bodyStyle.css"
 
 const App = () => {
   
   return (
     <>
       <Header/>
-      <Card number="#001" name="bulba" type1="tipo1" type2="tipo2" weight="7kg" height="1.8m"/>
-      <Card number="#001" name="bulba" type1="tipo1" type2="tipo2" weight="7kg" height="1.8m"/>
+      <div className="main_container">
+        <div className="container_items">
+          <Card number="#001" name="bulba" type1="tipo1" type2="tipo2" weight="7kg" height="1.8m"/>
+          <Card number="#001" name="bulba" type1="tipo1" type2="tipo2" weight="7kg" height="1.8m"/>
+          <Card number="#001" name="bulba" type1="tipo1" type2="tipo2" weight="7kg" height="1.8m"/>
+          <Card number="#001" name="bulba" type1="tipo1" type2="tipo2" weight="7kg" height="1.8m"/>
+          <Card number="#001" name="bulba" type1="tipo1" type2="tipo2" weight="7kg" height="1.8m"/>
+          <Card number="#001" name="bulba" type1="tipo1" type2="tipo2" weight="7kg" height="1.8m"/>
+        </div>
+      </div>
+      
     </>
   )
 }
